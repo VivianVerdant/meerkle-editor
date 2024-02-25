5d4428214cbc9e34d1c1eb20ab15722971bd491b
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2024-02-25 17:00:24 -0600

    bidirectional

--- meerkle-editor.js
+++ meerkle-editor.js
@@ -5,6 +5,7 @@ var meerkle_spacing_input = document.getElementById("meerkle-spacing-input");
 var meerkle_spacing = 0;
 var romankle_textarea = document.getElementById("romankle-textarea");
 var meerkle_textarea = document.getElementById("meerkle-textarea");
+var text_string = "";
 
 
 meerkle_size_input.oninput = (value) => {meerkle_size = meerkle_size_input.value; update_textareas()};
@@ -12,8 +13,12 @@ meerkle_size_input.oninput = (value) => {meerkle_size = meerkle_size_input.value
 meerkle_spacing_input.oninput = (value) => {meerkle_spacing = meerkle_spacing_input.value; update_textareas()};
 
 function update_textareas() {
+	romankle_textarea.value = text_string;
+	meerkle_textarea.value = text_string;
+
 	let str = `font-size: ${meerkle_size};letter-spacing: ${meerkle_spacing}rem;`;
 	meerkle_textarea.setAttribute("style", str);
+	
 }
 
 for (button of meerkle_buttons) {
@@ -26,3 +31,6 @@ function meerkle_button_click(event) {
 	romankle_textarea.value = romankle_textarea.value.concat(char);
 	meerkle_textarea.value = meerkle_textarea.value.concat(char);
 }
+
+romankle_textarea.addEventListener("update", (event) => {text_string =  romankle_textarea.value; update_textareas})
+meerkle_textarea.addEventListener("update", (event) => {text_string =  meerkle_textarea.value; update_textareas})
