7b696f40e5f4a3df6dfb9ed77156b8cd9d3649ea
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2024-02-25 15:01:48 -0600

    funcdate

--- meerkle-editor.js
+++ meerkle-editor.js
@@ -9,11 +9,11 @@ console.log(meerkle_buttons);
 
 meerkle_size_input = document.getElementById("meerkle-size-input");
 console.log(meerkle_size_input);
-meerkle_size_input.oninput((value) => {meerkle_size = value; update_textareas()});
+meerkle_size_input.oninput (value) => {meerkle_size = value; update_textareas()};
 
 meerkle_spacing_input = document.getElementById("meerkle-spacing-input");
 console.log(meerkle_spacing_input)
-meerkle_spacing_input.oninput((value) => {meerkle_spacing = value; update_textareas()});
+meerkle_spacing_input.oninput = (value) => {meerkle_spacing = value; update_textareas()};
 
 
 function update_textareas() {
