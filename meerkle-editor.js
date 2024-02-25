70d762f8ecb177ab77426367a2bd1e4b19e82750
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2024-02-25 14:40:52 -0600

    textarea elements

--- meerkle-editor.js
+++ meerkle-editor.js
@@ -1,5 +1,12 @@
-function main() {
-	var meerkle_buttons = document.querySelectorAll("button.meerkle");
-	console.log(meerkle_buttons);
-}
+var meerkle_buttons;
+var meerkle_size_input;
+var meerkle_spacing_input;
+
+meerkle_buttons = document.querySelectorAll("button.meerkle");
+console.log(meerkle_buttons);
+
+meerkle_size_input = document.getElementById("meerkle-size-input");
+console.log(meerkle_size_input);
+meerkle_spacing_input = document.getElementById("meerkle-spacing-input");
+console.log(meerkle_spacing_input)
 
